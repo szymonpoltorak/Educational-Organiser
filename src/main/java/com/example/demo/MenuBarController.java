8ab177e3d1dc0e98651f2f936289c7fa4b899e34
Szymon Poltorak <szymonpotorak@gmail.com>
2022-04-23 10:33:55 +0200

    Dodano bazowy folder projektu wraz z ikonami

---
+++ src/main/java/com/example/demo/MenuBarController.java
@@ -0,0 +1,55 @@
+package com.example.demo;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.*;
+import java.util.Objects;
+
+public class MenuBarController {
+    @FXML
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToSceneMainScene (MouseEvent event) {
+        try {
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Main.fxml")));
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void switchToSceneCalcScene (MouseEvent event) {
+        try {
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Calc.fxml")));
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void switchToSceneNoteScene (MouseEvent event) {
+        try {
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Note.fxml")));
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
